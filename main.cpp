afd017fecaf416584d1ff5c400913800902d9c81
op2000200 <op2000200@yandex.ru>
2024-10-26 17:31:40 +0700

    bugfix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
-double my_pow(double a, unsigned int b) 
-{
+#include <iostream>
+double my_pow(double a, int b) {
 	double c = a;
-	for (size_t i = 1; i < b; i++)
-		a = a * c;
+	if (b > 0)
+		for (size_t i = 1; i < b; i++)
+			a *= c;
+	else if (b == 0)
+		return 1;
+	else if (b < 0)
+		for (size_t i = 0; i <= abs(b); i++)
+			a *= (1 / c);
+
 	return a;
 }
